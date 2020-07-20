aaacb958137a2ffd85a6ebae2dff795489f72e3b
Nathan <sigh94@hotmail.com>
2020-07-19 20:31:13 -0400

    removing files

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [timer, setTime] = useState(0);
+
+    setTimeout(() => setTime(timer + 1), 1000 );
+
+    return <div >{timer}</div>
 }
 
 export default App;
